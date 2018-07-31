faffe1a0f3992e86bc1fe7804c8d2daad6352236
Richard Norris <richardnorris@Richards-MacBook-Pro.local>
2018-07-31 15:11:41 +0200

    Add Users class and updateUserList event

--- public/js/chat.js
+++ public/js/chat.js
@@ -17,7 +17,16 @@ function scrollToBottom (){
 }
 
      socket.on('connect', function () {
-               console.log('Connected to server');
+               var params = jQuery.deparam(window.location.search);
+
+               socket.emit('join', params, function (err) {
+                if(err){
+                    alert(err);
+                    window.location.href = '/';
+                } else {
+                    console.log('No error');
+                }
+               });
             });
 
             
@@ -26,9 +35,16 @@ function scrollToBottom (){
                 console.log('Disconnected from server');
             });
 
-socket.on('newEmail', function(email) {
-    console.log('New email', email);
+socket.on('updateUserList', function (users) {
+    var ol = jQuery('<ol></ol>');
+
+    users.forEach(function (user) {
+        ol.append(jQuery('<li></li>').text(user));
+    });
+
+    jQuery('#users').html(ol);
 });
+
 
 socket.on('newMessage', function (message) {
     var formattedTime = moment(message.createdAt).format('h:mm a');
